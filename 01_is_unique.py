19e05a94f6a31461b596585259f9589a0fdff1fe
Hoai An Phan <pnhoaian2904@gmail.com>
2022-06-20 04:14:50 +0000

    is_unique

---
+++ 01_is_unique.py
@@ -0,0 +1,32 @@
+""" Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures? 
+"""
+
+def is_unique(string):
+    """ Return True if the string has all unique characters. Return False otherwise. """
+
+    # 1. First approach:
+    # Convert the string to a set and compare the length of the string vs the length of the set
+    # If the same length -> unique characters -> True. Otherwise, False
+
+    # return len(string) == len(set(string))
+
+
+    # 2. Second approach: Not using additional data structures
+    # Iterate through each char, checking if it is in the remaining of the string by slicing
+
+    for i, char in enumerate(string):
+        if char in string[i+1:]:
+            return False
+    
+    return True
+
+
+if __name__ == "__main__":
+    # Test for the first approach
+    assert is_unique("howdy") == True
+    assert is_unique("hello") == False
+    assert is_unique("") == True
+    assert is_unique("hi you") == True
+    assert is_unique("Dod") == True
+    print("All tests passed.")
